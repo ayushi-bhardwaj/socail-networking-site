f1d5a2adb174f5e8fe6fd702511315be1d994f8b
unknown <ayushibhardwaj288@gmail.com>
2020-06-23 15:11:31 +0530

    set up static file access

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express= require('express');
 const app=express();
 const port=8000;
 const router=require('./routes/index');
-
-
-
+const expressLayouts=require('express-ejs-layouts')
+app.use(express.static('./assets'))
+app.use(expressLayouts)
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 app.use('/',router);
+
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err)
     {
