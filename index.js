f927e414dd5ed45d04d923f553b9abf5d6c705bc
unknown <ayushibhardwaj288@gmail.com>
2020-06-25 12:02:30 +0530

    authentication

--- index.js
+++ index.js
@@ -4,19 +4,45 @@ const app=express();
 const port=8000;
 const router=require('./routes/index');
 const expressLayouts=require('express-ejs-layouts')
-const db=require('./config/mongoose')
-
-
+const db=require('./config/mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+const { MongoStore } = require('connect-mongo');
+const mongoStore=require('connect-mongo')(session);
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('./assets'))
 app.use(expressLayouts)
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
-app.use('/',router);
+
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name:'codial',
+    secret:'ayushiisbest',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000*60*100)
+    },
+    store: new mongoStore({
+        
+            mongooseConnection:db,
+            autoRemove:'disabled'
+        
+    },function(err){
+        console.log(err||'connect-mongodb setup okay')
+    })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser)
+app.use('/',router);
 app.listen(port,function(err){
     if(err)
     {
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 const express= require('express');
 const router=express.Router();
-
+const passport=require('../config/passport-local-strategy')
 const usersController=require('../controllers/users_controller');
 const postsController=require('../controllers/posts_controller')
-router.get('/profile',usersController.profile)
+router.get('/profile',passport.checkAuthentication,usersController.profile)
 
 router.get('/posts',postsController.posts)
 router.get('/sign-in',usersController.signIn);
 router.get('/sign-up',usersController.signUp)
 router.post('/create',usersController.create);
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'}
+),usersController.createSession)
 module.exports=router
